dbd1eee1d8726ed2c095b209fb96e86049c8be3a
unknown <>
2017-01-15 07:12:52 +0900

    通信

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ port: process.env.PORT });
-var http = require( 'http' );
-var server = http.createServer( function( req, res ) {
-  //もしURLにファイル名がないならばindex.htmlに飛ばすように
-  if(req.url == "/")
-    req.url = "/index.html";
-  //URLでリクエストされたページをread
-  fs.readFile(__dirname + req.url, 'utf-8', function(err, data){
-    //もし見つからなかったら404を返す
-    if(err){　//err=trueならNot Foundを返します。
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.write("Not Found");
-      return res.end();　
-    }
-    //見つかったら表示
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
-});server.listen(process.env.PORT)
 
 console.log("listening port :" + process.env.PORT);
 var connections = [];
